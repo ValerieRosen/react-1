6ec2ca0696c08b8ecbf080b81081d43e8d2013bf
Valerie R <vrosenhauer@gmail.com>
2024-01-28 23:24:05 -0500

    updated function/tests

--- addCommas.js
+++ addCommas.js
@@ -2,16 +2,51 @@
 // and converts it into a string formatted with commas added
 // for readability
 
-function addCommas() {
-  const num = [""];
-  num[1] = 1234;
-  num[2] = 1000000;
-  num[3] = 9876543210;
-  num[4] = 6;
-  num[5] = -10;
-  num[6] = -5678;
+// function addCommas(num) {
+//   const num = [""];
+//   num[1] = 1234;
+//   num[2] = 1000000;
+//   num[3] = 9876543210;
+//   num[4] = 6;
+//   num[5] = -10;
+//   num[6] = -5678;
 
-  return [num.toLocaleString()];
+//   return [num.toLocaleString()];
+// }
+
+function addCommas(num) {
+  //All numbers + but if - remember sign
+  let sign = "";
+  if (num < 0) sign = "-";
+  num = Math.abs(num);
+
+  //Convert to a string and split on either side of the decimal
+  let [whole, decimal] = num.toString().split(".");
+
+  if (decimal) {
+    return `${sign}${commaInsert(whole)}.${decimal}`;
+  }
+
+  return `${sign}${commaInsert(whole)}`;
+}
+
+function commaInsert(str) {
+  //keep track of where comma should go
+  let commaIndex = 0;
+  let commaStr = "";
+
+  //iterate backwards
+  for (let i = str.length - 1; i >= 0; i--) {
+    commaStr = str[i] + commaStr;
+    commaIndex++;
+
+    //add comma after every 3rd character except beginning
+    if (commaIndex % 3 === 0 && i !== 0) {
+      commaStr = "," + commaStr;
+    }
+  }
+
+  return commaStr;
 }
 
 module.exports = addCommas;
--- addCommas.test.js
+++ addCommas.test.js
@@ -5,23 +5,33 @@ describe("#addCommas", () => {
     expect(typeof addCommas).toBe("function");
   });
 
-  test("is there a comma", () => {
-    expect("1,000,000").toBe("1,000,000");
+  test("it works for positive numbers", () => {
+    expect(addCommas(6)).toBe("6");
+    expect(addCommas(89)).toBe("89");
+    expect(addCommas(988)).toBe("988");
   });
 
-  test("is there a comma", () => {
-    expect("9,876,543,210").toBe("9,876,543,210");
+  test("it works for positive large numbers", () => {
+    expect(addCommas(9876543210)).toBe("9,876,543,210");
+    expect(addCommas(16000210)).toBe("16,000,210");
+    expect(addCommas(1000000)).toBe("1,000,000");
   });
 
-  test("is there a comma", () => {
-    expect("6").toBe("6");
+  test("it works for negative small numbers", () => {
+    expect(addCommas(-6)).toBe("-6");
+    expect(addCommas(-57)).toBe("-57");
+    expect(addCommas(-124)).toBe("-124");
   });
 
-  test("is there a comma", () => {
-    expect("-10").toBe("-10");
+  test("it works for negative large numbers", () => {
+    expect(addCommas(-6344)).toBe("-6,344");
+    expect(addCommas(-34567)).toBe("-34,567");
+    expect(addCommas(-78999456)).toBe("-78,999,456");
   });
 
-  test("is there a comma", () => {
-    expect("-5,678").toBe("-5,678");
+  test("it works for small positive decimals", () => {
+    expect(addCommas(1.234)).toBe("1.234");
+    expect(addCommas(8.99)).toBe("8.99");
+    expect(addCommas(477.99088)).toBe("477.99088");
   });
 });

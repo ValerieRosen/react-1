0fda28bdaf71f86d46050cc030b2dacb66af2d9a
Valerie R <vrosenhauer@gmail.com>
2024-01-28 19:12:40 -0500

    added new tests

--- addCommas.test.js
+++ addCommas.test.js
@@ -4,4 +4,24 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+
+  test("is there a comma", () => {
+    expect("1,000,000").toBe("1,000,000");
+  });
+
+  test("is there a comma", () => {
+    expect("9,876,543,210").toBe("9,876,543,210");
+  });
+
+  test("is there a comma", () => {
+    expect("6").toBe("6");
+  });
+
+  test("is there a comma", () => {
+    expect("-10").toBe("-10");
+  });
+
+  test("is there a comma", () => {
+    expect("-5,678").toBe("-5,678");
+  });
 });
